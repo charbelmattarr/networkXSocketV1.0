63bec22dd935ffac0e86feadd5be0711945614de
charbelmattarr <74071943+charbelmattarr@users.noreply.github.com>
2023-02-27 23:31:04 +0200

    second

--- Node.py
+++ Node.py
@@ -1,43 +1,44 @@
 import zmq
 import threading
+
+
 class Node:
-    def __init__(self, port):
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.SUB)
-        self.socket.connect(f"tcp://localhost:{port}")
-        self.socket.subscribe(b"")
+    def __init__(self, port, neighbors=None):
         self.port = port
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.bind(f"tcp://*:{self.port}")
+        self.sub_socket = self.context.socket(zmq.SUB)
+        self.sub_socket.connect(f"tcp://localhost:{self.port}")
+        self.sub_socket.subscribe(b"")
         self.received_messages = []
+        self.listening_thread = None
+        self.listening = False
 
-        self.send_socket = self.context.socket(zmq.PUB)
-        self.send_socket.bind(f"tcp://*:{port + 1}")
-
-        self.is_running = False
+        self.neighbors = neighbors or []
 
     def start_listening(self):
-        if not self.is_running:
-            self.is_running = True
-            self.listen_thread = threading.Thread(target=self._listen, daemon=True)
-            self.listen_thread.start()
+        if self.listening:
+            return
+        self.listening = True
+        self.listening_thread = threading.Thread(target=self.receive_messages)
+        self.listening_thread.start()
 
     def stop_listening(self):
-        self.is_running = False
+        self.listening = False
+        self.listening_thread.join()
 
-    def read_data(self):
-        while True:
-            message = self.socket.recv()
-            self.received_messages.append(message)
-
-    def _listen(self):
-        while self.is_running:
+    def receive_messages(self):
+        while self.listening:
             try:
-                message = self.socket.recv_string()
-                print(f"Received message: {message}")
-            except zmq.error.ZMQError:
+                message = self.sub_socket.recv_string(flags=zmq.NOBLOCK)
+                self.received_messages.append(message)
+                print(f"Received message on port {self.port}: {message}")
+            except zmq.ZMQError:
                 pass
 
-
-
     def send_message(self, message):
-        self.send_socket.send_string(message)
-
+        self.socket.send_string(message)
+        # broadcast the message to neighbors
+        for neighbor in self.neighbors:
+            neighbor.send_message(message)
--- main.py
+++ main.py
@@ -4,27 +4,23 @@ import time
 import zmq
 import threading
 from Node import Node
+# create three nodes on ports 5555, 5556, and 5557, with appropriate neighbors
+
+node1 = Node(5558, None)
+node2 = Node(5559, None)
+node3 = Node(5560, None)
 
 
-node1 = Node(5558)
-node2 = Node(5556)
-
-# start listening for incoming messages on both nodes
+node1.neighbors=[node2, node3]
+node2.neighbors=[node1, node3]
+node3.neighbors=[node1, node2]
+# start listening for incoming messages on all nodes
 node1.start_listening()
 node2.start_listening()
+node3.start_listening()
 
-# send a message from node1 to node2
+# send a message from node1 to all neighbors
 node1.send_message("Hello from node 1!")
-
-# send a message from node2 to node1
-node2.send_message("Hello from node 2!")
-
-# wait for a few seconds for messages to be received
-time.sleep(2)
-
-# print received messages for each node
-print(f"Node 1 received: {node1.received_messages}")
-print(f"Node 2 received: {node2.received_messages}")
 
 # stop listening for incoming messages on node2
 node2.stop_listening()
